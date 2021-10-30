145c4a2325e587e975c7edc1469cf46f6c946951
jasonc005 <jason.chandlerp@gmail.com>
2021-10-30 16:42:48 -0400

    Add project files.

---
+++ HA2_1/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace HA2_1
+{
+    partial class searchDBForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameTextbox = new System.Windows.Forms.TextBox();
+            this.ageTextbox = new System.Windows.Forms.TextBox();
+            this.genderTextbox = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.ageLabel = new System.Windows.Forms.Label();
+            this.genderLabel = new System.Windows.Forms.Label();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.resultListbox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // nameTextbox
+            // 
+            this.nameTextbox.Location = new System.Drawing.Point(187, 28);
+            this.nameTextbox.Name = "nameTextbox";
+            this.nameTextbox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextbox.TabIndex = 0;
+            // 
+            // ageTextbox
+            // 
+            this.ageTextbox.Location = new System.Drawing.Point(187, 76);
+            this.ageTextbox.Name = "ageTextbox";
+            this.ageTextbox.Size = new System.Drawing.Size(100, 20);
+            this.ageTextbox.TabIndex = 1;
+            // 
+            // genderTextbox
+            // 
+            this.genderTextbox.Location = new System.Drawing.Point(187, 132);
+            this.genderTextbox.Name = "genderTextbox";
+            this.genderTextbox.Size = new System.Drawing.Size(100, 20);
+            this.genderTextbox.TabIndex = 2;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(56, 35);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(92, 13);
+            this.nameLabel.TabIndex = 3;
+            this.nameLabel.Text = "First or Last Name";
+            // 
+            // ageLabel
+            // 
+            this.ageLabel.AutoSize = true;
+            this.ageLabel.Location = new System.Drawing.Point(56, 83);
+            this.ageLabel.Name = "ageLabel";
+            this.ageLabel.Size = new System.Drawing.Size(26, 13);
+            this.ageLabel.TabIndex = 4;
+            this.ageLabel.Text = "Age";
+            // 
+            // genderLabel
+            // 
+            this.genderLabel.AutoSize = true;
+            this.genderLabel.Location = new System.Drawing.Point(56, 139);
+            this.genderLabel.Name = "genderLabel";
+            this.genderLabel.Size = new System.Drawing.Size(92, 13);
+            this.genderLabel.TabIndex = 5;
+            this.genderLabel.Text = "Gender (m/M/f/F)";
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(150, 185);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 6;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // resultListbox
+            // 
+            this.resultListbox.FormattingEnabled = true;
+            this.resultListbox.Location = new System.Drawing.Point(59, 240);
+            this.resultListbox.Name = "resultListbox";
+            this.resultListbox.Size = new System.Drawing.Size(228, 95);
+            this.resultListbox.TabIndex = 7;
+            // 
+            // searchDBForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 408);
+            this.Controls.Add(this.resultListbox);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.genderLabel);
+            this.Controls.Add(this.ageLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.genderTextbox);
+            this.Controls.Add(this.ageTextbox);
+            this.Controls.Add(this.nameTextbox);
+            this.Name = "searchDBForm";
+            this.Text = "Search Database";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameTextbox;
+        private System.Windows.Forms.TextBox ageTextbox;
+        private System.Windows.Forms.TextBox genderTextbox;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label ageLabel;
+        private System.Windows.Forms.Label genderLabel;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.ListBox resultListbox;
+
+    }
+}
+
---
+++ HA2_1/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace HA2_1
+{
+    public partial class searchDBForm : Form
+    {
+        
+        public searchDBForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            resultListbox.Items.Clear();
+
+            string name = "";
+            int age = 0;
+            string gender = "";
+
+            if (nameTextbox.Text != "")
+            {
+                name = nameTextbox.Text;
+
+                if (ageTextbox.Text != "")
+                {
+                    if (int.TryParse(ageTextbox.Text, out age))
+                    {
+                        if (age > 0)
+                        {
+                            age = int.Parse(ageTextbox.Text);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Provide a valid positive integer as age. Age is not being considered for search.");
+                        }
+                    }
+                }
+
+                if (genderTextbox.Text != "")
+                {
+                    if (genderTextbox.Text.ToUpper() == "M" || genderTextbox.Text.ToUpper() == "F")
+                    {
+                        gender = genderTextbox.Text.ToUpper();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Provide a valid gender (m,M,f,F). Gender is not being considered in the search.");
+                    }
+                }
+
+                StreamReader dbInput = File.OpenText("LeaderData.txt");
+
+                while (!dbInput.EndOfStream)
+                {
+                    string line = dbInput.ReadLine();
+                    string tempName = "";
+                    int tempAge = 0;
+                    string tempGender = "";
+
+                    getDataFromLine(line, ref tempName, ref tempAge, ref tempGender);
+
+                    if (tempName.ToUpper().IndexOf(name.ToUpper()) != -1)
+                    {
+                        if(gender != "" && age > 0 && gender == tempGender && age == tempAge)
+                        {
+                            resultListbox.Items.Add("Name=" + tempName + ", Age=" + tempAge + ", Gender=" + tempGender + "\n");
+                        }
+                        else if (gender != "" && age == 0 && gender == tempGender)
+                        {
+                            resultListbox.Items.Add("Name=" + tempName + ", Age=" + tempAge + ", Gender=" + tempGender + "\n");
+                        }
+                        else if (gender == "" && age > 0 && age == tempAge)
+                        {
+                            resultListbox.Items.Add("Name=" + tempName + ", Age=" + tempAge + ", Gender=" + tempGender + "\n");
+                        }
+                        else if (gender == "" && age == 0)
+                        {
+                            resultListbox.Items.Add("Name=" + tempName + ", Age=" + tempAge + ", Gender=" + tempGender + "\n");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Name is necessary to initiate the search.");
+            }
+        }
+
+        //Here goes the method to set the name, age and gender from the line.
+        void getDataFromLine(string line, ref string name, ref int age, ref string gender)
+        {
+            int nameComma = line.IndexOf(",");
+            int ageComma = line.IndexOf(",", nameComma + 1);
+            name = line.Substring(0, nameComma);
+            age = int.Parse(line.Substring(nameComma + 1, ageComma - nameComma - 1));
+            gender = line.Substring(ageComma + 1);
+        }
+    }
+}
